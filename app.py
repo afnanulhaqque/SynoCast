fed7832b75c23ec4aa2ac5958de89dd153c2581a
Afnan Ul Haq <afnanulhaq4@gmail.com>
2025-11-14 16:12:22 +0500

    Add routes for news, weather, and about pages; update base template and styles
    Add navbar and footer, assests

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, redirect , render_template , request, url_for
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route('/home')
+
+app = Flask(
+    __name__,
+    template_folder="templates",
+    static_folder="assests",
+    static_url_path="/assests",
+)
+
+
+@app.route("/")
 def home():
-    return render_template('home.html')    
+    return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run(debug=True , port=8000)
+
+@app.route("/news")
+def news():
+    return render_template("news.html")
+
+
+@app.route("/weather")
+def weather():
+    return render_template("weatherApp.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("About.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
