b35f6b3a999589051807240dfefc9fa1e5b3df9d
Afnan Ul Haq <afnanulhaq4@gmail.com>
2025-12-19 02:12:22 +0500

    chore: remove syntax check script for weather.js

--- check_syntax.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('assests/js/weather.js', 'utf8');
-try {
-    new Function(content);
-    console.log('Syntax OK');
-} catch (e) {
-    console.error(e);
-}
